0906ae2a8e07472340b21877b3e66ef3732dac28
Tupi Guedes <tupiguedes@outlook.com>
2023-03-16 11:51:49 -0300

    Iniciando projeto

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Alert, Button, SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [texto, onChangeTexto] = React.useState('')
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <SafeAreaView>
+        <Text>Buscar...</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeTexto}
+          value={texto}
+        />
+      </SafeAreaView>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  input: {
+    height: 40,
+    margin: 25,
+    borderWidth: 0.3,
+    padding: 10,
+    borderRadius: 10
   },
 });
